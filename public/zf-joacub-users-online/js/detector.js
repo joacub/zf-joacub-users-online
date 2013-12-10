de29b26c1a944d6fb9cfc4015f6ff99320cc7f57
Johan <joacub@gmail.com>
2013-12-10 22:59:21 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- public/zf-joacub-users-online/js/detector.js
+++ public/zf-joacub-users-online/js/detector.js
@@ -6,15 +6,21 @@ jQuery(document).ready(function($) {
 	intervalUserOnline = setInterval(function() {
 		if(abortDetector)
 			return;
+		
+		abortDetector = true;
 		var date = new Date();
 		xhrUsersOnline = $.ajax({
 			url : site_url + "/users-online-detector?"+date.getTime(),
+			complete:function() {
+				abortDetector = false;
+			}
 		});
 	}, 10000);
 	
 	$(window).bind('beforeunload',function(){
 		clearInterval(intervalUserOnline);
 		abortDetector = true;
+		if(typeof(xhrUsersOnline) != 'undefined')
 		xhrUsersOnline.abort();
 
 	});
